9e2ed37b4c5b85e90640dc21510759769c68527b
MrDSGC <dcheng47@gmail.com>
2017-04-04 20:56:30 -0700

    added background to splashpage

--- src/components/UserShow.js
+++ src/components/UserShow.js
@@ -7,7 +7,10 @@ class UserShow extends React.Component {
   render () {
     return (
       <View>
+        <Text>Email:</Text>
         <Text>{firebase.auth().currentUser.email}</Text>
+        <Text>About:</Text>
+        <Text>Scriblr is a mobile note taking application made with react-native in 1 week. </Text>
         <CardSection>
           <Button
             onPress={() => console.log("logout")}>
